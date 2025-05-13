20d497cb59d044136be89a76c825b1862a5ee2f6
B0gus <melvinsiadous@icloud.com>
2025-05-13 15:12:54 +0000

    Add Hello World application and unit tests

---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+Module simple affichant un message Hello World pour le laboratoire LOG430.
+"""
+
+def main():
+    """Fonction principale qui affiche le message Hello World."""
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
